6bd9ebd1279c9d2ea3644e017c4feaa34f19a8ab
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-11-30 14:48:21 +0000

    feat: add prompt (#2)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod command;
 use clap::Parser;
+use dialoguer::{theme::ColorfulTheme, MultiSelect};
+use include_dir::{include_dir, Dir};
 use std::{
     fs::create_dir_all,
     fs::{self},
@@ -22,17 +24,49 @@ fn main() {
 }
 
 fn handle_create_command(path: String) -> () {
-    let template = include_str!("../templates/+page.svelte.template");
-    let target_path = format!("src/routes/{}/+page.svelte", path);
+    static TEMPLATES: Dir = include_dir!("templates");
+
+    let multiselected = &[
+        "+page.svelte",
+        "+page.ts",
+        "+page.server.ts",
+        "+layout.svelte",
+        "+layout.ts",
+        "+layout.server.ts",
+        "+server.ts",
+        "+error.svelte",
+    ];
+    let defaults = &[true, false, false];
+
+    let selections = MultiSelect::with_theme(&ColorfulTheme::default())
+        .with_prompt("Pick your types")
+        .items(&multiselected[..])
+        .defaults(&defaults[..])
+        .interact()
+        .unwrap();
+
+    if selections.is_empty() {
+        println!("No types selected. Aborting...");
+        return;
+    }
 
     create_dir_all(format!("src/routes/{}", path)).expect("failed to create directory");
 
-    let mut file = fs::OpenOptions::new()
-        .create(true)
-        .write(true)
-        .open(target_path)
-        .expect("failed to create file");
+    for selection in selections {
+        let template = TEMPLATES
+            .get_file(format!("{}.template", multiselected[selection]))
+            .unwrap()
+            .contents_utf8()
+            .unwrap();
 
-    file.write_all(template.as_bytes())
-        .expect("failed to write file");
+        let target_path = format!("src/routes/{}/{}", path, multiselected[selection]);
+        let mut file = fs::OpenOptions::new()
+            .create(true)
+            .write(true)
+            .open(target_path)
+            .expect("failed to create file");
+
+        file.write_all(template.as_bytes())
+            .expect("failed to write file");
+    }
 }
